bc285868f413609f71bf669d0e00bccd1c3f90ba
Suji <suji9709@gmail.com>
2022-08-09 14:46:05 +0900

    [Add] post, get, delete API 구현 완료

--- src/todo/controller/todo.controller.ts
+++ src/todo/controller/todo.controller.ts
@@ -1,14 +1,55 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Delete,
+  Post,
+  Put,
+  Param,
+  Body,
+} from '@nestjs/common';
 import { TodoService } from '../service/todo.service';
 import { Todo } from '@prisma/client';
 
-@Controller('api/vi/todos')
+@Controller('api/v1/todos')
 export class TodoController {
   // constructor: 생성자
   constructor(private readonly todoService: TodoService) {}
 
   @Get()
+  // 전체 조회
   async fetchAllTodos(): Promise<Todo[]> {
     return this.todoService.fetchAllTodos();
+  }
+
+  // 한개 조회
+  @Get(':id') // parameter
+  async fetchTodoItem(@Param('id') id: number): Promise<Todo | null> {
+    return this.todoService.fetchTodoItem(id);
+  }
+
+  // 삭제
+  @Delete(':id')
+  async deleteTodoItem(@Param('id') id: number): Promise<Todo | null> {
+    return this.todoService.deleteTodoItem(id);
+  }
+
+  // 입력
+  @Post()
+  async addTodoItem(@Body() data: Todo): Promise<Todo> {
+    return this.todoService.addTodoItem(data);
+  }
+
+  // 수정
+  @Put(':id')
+  async updateTodoItem(
+    @Param('id') id: number,
+    @Body() data: Todo,
+  ): Promise<Todo | null> {
+    return this.todoService.updateTodoItem(
+      id,
+      data.title,
+      data.content,
+      data.is_done,
+    );
   }
 }
--- src/todo/service/todo.service.ts
+++ src/todo/service/todo.service.ts
@@ -11,4 +11,32 @@ export class TodoService {
     //비동기적으로 받아주도록
     return this.prismaService.todo.findMany();
   }
+
+  // 한개 조회
+  async fetchTodoItem(id: number): Promise<Todo | null> {
+    return this.prismaService.todo.findUnique({ where: { id: Number(id) } });
+  }
+
+  // 삭제
+  async deleteTodoItem(id: number): Promise<Todo | null> {
+    return this.prismaService.todo.delete({ where: { id: Number(id) } });
+  }
+
+  // 할일 입력
+  async addTodoItem(data: Todo): Promise<Todo> {
+    return this.prismaService.todo.create({ data: data });
+  }
+
+  // 수정
+  async updateTodoItem(
+    id: number,
+    title: string,
+    content: string,
+    is_done: boolean,
+  ): Promise<Todo | null> {
+    return this.prismaService.todo.update({
+      where: { id: Number(id) },
+      data: { title: title, content: content, is_done: is_done },
+    });
+  }
 }
